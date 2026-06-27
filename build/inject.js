748855792181f8d607f30933e1069ee491ed0def
H3l!0s_T3k <90980178+Ismail-Benali@users.noreply.github.com>
2026-06-27 14:28:10 +0100

    Delete build directory

--- build/inject.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-(() => {
-  (() => {
-    let script = document.createElement("script");
-    script.src = chrome.runtime.getURL("build/script.js");
-    document.documentElement.appendChild(script);
-    script.remove();
-  })();
-})();
